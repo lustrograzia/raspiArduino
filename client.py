3d25211a71b5df8b942ba86cb8c7761872ae77d2
system10 <lustrograzia@gmail.com>
2019-11-04 18:04:47 +0900

    img transfer

--- client.py
+++ client.py
@@ -1,10 +1,39 @@
 
 import socket
+import numpy as np
+import cv2 as cv
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-ip = '10.10.23.10'
-port = 8000
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
 
-client_socket.sendto(b'hello', (ip, port))
-data = client_socket.recvfrom(55555)
+
+IP = '10.10.23.10'
+PORT = 8000
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+
+while True:
+
+    message = '1'
+    client_socket.send(message.encode())
+
+    length = recvall(client_socket, 16)
+    stringData = recvall(client_socket, int(length))
+    data = np.frombuffer(stringData, dtype=np.uint8)
+
+    decimg = cv.imdecode(data, 1)
+    cv.imshow('image', decimg)
+
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+client_socket.close()
--- server.py
+++ server.py
@@ -1,22 +1,78 @@
 
 import socket
+import cv2 as cv
+import numpy
+from queue import Queue
+from _thread import *
 
-ip = '10.10.23.10'
-port = 8000
+enclosure_queue = Queue()
 
-# Start a socket listening for connections on 0.0.0.0:8000
-# (0.0.0.0 means all interfaces)
 
-# AF_INET : IPv4, SOCK_DGRAM : UDP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((ip, port))
+# thread function
+def threaded(client_socket, addr, queue):
+    print('connected by :', addr[0], ':', addr[1])
+    while True:
+        try:
+            data = client_socket.recv(1024)
+
+            if not data:
+                print('disconnected by ' + addr[0], ':', addr[1])
+                break
+
+            stringData = queue.get()
+            client_socket.send(str(len(stringData)).ljust(16).encode())
+            client_socket.send(stringData)
+
+        except ConnectionResetError as e:
+            print('Disconnected by ' + addr[0], ':', addr[1])
+            break
+
+    client_socket.close()
+
+
+def webcam(queue):
+
+    capture = cv.VideoCapture(-1)
+
+    while True:
+        ret, frame = capture.read()
+
+        if ret == False:
+            continue
+
+        encode_param = [int(cv.IMWRITE_JPEG_QUALITY), 90]
+        result, imgencode = cv.imencode('.jpg', frame, encode_param)
+
+        data = numpy.array(imgencode)
+        stringData = data.tostring()
+
+        queue.put(stringData)
+
+        cv.imshow('image', frame)
+
+        key = cv.waitKey(1)
+        if key == 27:
+            break
+
+
+IP = '10.10.23.10'
+PORT = 8000
+
+# AF_INET : IPv4, SOCK_DGRAM : UDP, SOCK_STREAM : TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+server_socket.listen()
 
 print('server start')
-print('server ip: {:s} | port: {:d}'.format(ip, port))
+print('server ip: {:s} | port: {:d}'.format(IP, PORT))
+
+start_new_thread(webcam, (enclosure_queue,))
 
 while True:
-    data, info = server_socket.recvfrom(55555)
-    print('client: {:s}/{:d}'.format(info[0], info[1]))
-    print('data: {:s}'.format(data.decode()))
+    print('wait')
 
-    server_socket.sendto(data, info)
+    client_socket, addr = server_socket.accept()
+    start_new_thread(threaded, (client_socket, addr, enclosure_queue,))
+
+server_socket.close()
